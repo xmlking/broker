a5c03765d911c07705f33593f84443cd3f5cabc6
Sumanth Chinthagunta <xmlking@gmail.com>
2020-07-01 23:08:57 -0700

    build(clog): updating changelog

---
+++ pubsub/cmd/emulator/README.md
@@ -0,0 +1,7 @@
+## How to Run
+
+```
+docker-compose up pub-sub-emulator
+```
+
+and then from a different terminal: `go test -v ./pubsub/cmd/emulator`
---
+++ pubsub/cmd/publish/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"cloud.google.com/go/pubsub"
+	"github.com/google/uuid"
+	"github.com/rs/zerolog/log"
+
+	"github.com/xmlking/broker/pubsub"
+)
+
+
+func main() {
+	// broker.DefaultBroker = broker.NewBroker(broker.ProjectID("my-project-id")); // use cfg.pubsub.ProjectID
+	broker.DefaultBroker = broker.NewBroker()
+
+	msg := pubsub.Message{
+		ID:         uuid.New().String(),
+		Data:       []byte("ABC€"),
+		Attributes: map[string]string{"sumo": "demo"},
+	}
+
+	if err := broker.Publish("ingestion-in-dev", &msg); err != nil {
+		log.Error().Err(err).Send()
+	}
+}
